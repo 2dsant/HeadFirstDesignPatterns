79ee68a36aaa9928e50f982bcb624e4a61e23853
David Santos <2dsant3@gmail.com>
2025-04-16 21:25:09 -0300

    Adicionando o livro de referência

---
+++ src/Chapter01/HeadFirstDesignPatterns.Chapter01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HeadFirstDesignPatterns.Chapter01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
